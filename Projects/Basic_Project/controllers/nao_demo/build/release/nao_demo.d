9a21500fd08eee7324e9bd7fe25a9eb935b71663
WayneWeiOvO <13164674313@163.com>
2023-10-26 18:57:09 +0100

    1. 工程环境开发；2. 运动控制step1  开发时间20231026
    Co-Authored-By: FennMai <147424568+FennMai@users.noreply.github.com>

---
+++ Projects/Basic_Project/controllers/nao_demo/build/release/nao_demo.d
@@ -0,0 +1,17 @@
+build/release/nao_demo.o: nao_demo.c \
+ D:\Webots/include/controller/c/webots/accelerometer.h \
+ D:\Webots/include/controller/c/webots/types.h \
+ D:\Webots/include/controller/c/webots/camera.h \
+ D:\Webots/include/controller/c/webots/camera_recognition_object.h \
+ D:\Webots/include/controller/c/webots/distance_sensor.h \
+ D:\Webots/include/controller/c/webots/gps.h \
+ D:\Webots/include/controller/c/webots/gyro.h \
+ D:\Webots/include/controller/c/webots/inertial_unit.h \
+ D:\Webots/include/controller/c/webots/keyboard.h \
+ D:\Webots/include/controller/c/webots/led.h \
+ D:\Webots/include/controller/c/webots/motor.h \
+ D:\Webots/include/controller/c/webots/robot.h \
+ D:\Webots/include/controller/c/webots/nodes.h \
+ D:\Webots/include/controller/c/webots/touch_sensor.h \
+ D:\Webots/include/controller/c/webots/utils/motion.h \
+ D:/Webots/include/controller/c/webots/types.h
